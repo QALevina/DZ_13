05c2f29ed945b82277b776bb5f4f5004be55ae85
Elena Levina <qa-levina@yandex.ru>
2022-08-24 11:33:24 +0300

    Mission accomplished

---
+++ src/main/java/ru/netology/repozitory2/ProductRepozitory.java
@@ -0,0 +1,36 @@
+package ru.netology.repozitory2;
+
+import ru.netology.product2.Product;
+
+public class ProductRepozitory {
+    Product[] products = new Product[0];
+
+
+    public Product[] add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+        return products;
+    }
+
+    public void removeById(int removeId) {
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != removeId) {
+                tmp[copyToIndex] = product;
+                copyToIndex ++;
+            }
+            products = tmp;
+        }
+
+    }
+
+    public Product[] findAll() {
+        return products;
+    }
+}
